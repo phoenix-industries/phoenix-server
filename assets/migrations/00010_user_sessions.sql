b57c6367fbdb8c363148d9ead87a54ec60fc1be6
Osama Ragab <theosamaragab@gmail.com>
2026-03-09 15:17:24 +0200

    feat: add user sessions migrations

---
+++ assets/migrations/00010_user_sessions.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+CREATE TABLE user_sessions (
+	id TEXT NOT NULL PRIMARY KEY,
+	user_id TEXT REFERENCES users(id) ON DELETE SET NULL,
+	token TEXT NOT NULL,
+	ip_address TEXT NOT NULL,
+	user_agent TEXT NOT NULL,
+	expires_at TIMESTAMP with time zone NOT NULL,
+	created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	deleted_at TIMESTAMP WITH TIME ZONE
+);
+
+-- +goose Down
+DROP TABLE user_sessions;
