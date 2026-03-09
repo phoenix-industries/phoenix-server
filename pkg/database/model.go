0158e30508a4cece292e5d3017dbb8965b94abe6
Osama Ragab <theosamaragab@gmail.com>
2026-03-09 07:00:21 +0200

    feat(db): add user model

---
+++ pkg/database/model.go
@@ -0,0 +1,10 @@
+package database
+
+import "time"
+
+type Model struct {
+	ID        string     `db:"id" json:"id"`
+	CreatedAt *time.Time `db:"created_at" json:"created_at"`
+	UpdatedAt *time.Time `db:"updated_at" json:"updated_at"`
+	DeletedAt *time.Time `db:"deleted_at" json:"deleted_at"`
+}
