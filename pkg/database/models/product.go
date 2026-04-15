ac3eb43e823f94cbd2587f068f893ac5ff204007
Osama Ragab <theosamaragab@gmail.com>
2026-04-15 20:38:16 +0200

    feat: add product insert function

--- pkg/database/models/product.go
+++ pkg/database/models/product.go
@@ -16,16 +16,64 @@ type Product struct {
 	UserID       string     `db:"user_id" json:"user_id"`
 	Name         string     `db:"name" json:"name"`
 	Price        int        `db:"price" json:"price"`
-	CategoryID   string     `db:"category_id" json:"-"`
+	CategoryID   string     `db:"category_id" json:"category_id"`
 	Condition    string     `db:"condition" json:"condition"`
 	MinimumAge   int        `db:"minimum_age" json:"minimum_age"`
 	MaximumAge   int        `db:"maximum_age" json:"maximum_age"`
 	TargetGender string     `db:"target_gender" json:"target_gender"`
 	Description  string     `db:"description" json:"description"`
-	Tags         *string    `db:"tags" json:"tags"`
-	ReviewedAt   *time.Time `db:"reviewed_at" json:"reviewed_at"`
 	Approved     bool       `db:"approved" json:"approved"`
 	Category     string     `db:"category" json:"category"`
+	Tags         *string    `db:"tags" json:"tags,omitempty"`
+	ReviewedAt   *time.Time `db:"reviewed_at" json:"reviewed_at"`
+}
+
+func (p *Product) Validate() error {
+	if p.UserID == "" {
+		return errors.New("user id is required")
+	}
+	if p.CategoryID == "" {
+		return errors.New("product category is required")
+	}
+	if p.Name == "" {
+		return errors.New("product name is required")
+	}
+	if p.Condition == "" {
+		return errors.New("product condition is required")
+	}
+	if p.Price <= 0 {
+		return errors.New("product price must be greater than 0")
+	}
+	if p.MinimumAge < 3 {
+		return errors.New("minimum age must be greater than or equal to 3")
+	}
+	if p.MaximumAge < 0 {
+		return errors.New("maximum age must be greater than or equal to 0")
+	}
+	if p.MaximumAge > 0 && p.MaximumAge < p.MinimumAge {
+		return errors.New("maximum age must be greater than or equal to minimum age")
+	}
+	if p.TargetGender == "" {
+		return errors.New("target gender is required")
+	}
+	if p.Description == "" {
+		return errors.New("product description is required")
+	}
+	return nil
+}
+
+func ProductInsert(ctx context.Context, db database.DB, product *Product) error {
+	if err := product.Validate(); err != nil {
+		return err
+	}
+	query := `
+		INSERT INTO products
+		(id, user_id, name, price, category_id, condition, minimum_age, maximum_age, target_gender, description, tags, approved)
+		VALUES
+		($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, false)
+	`
+	_, err := db.Exec(ctx, query, product.ID, product.UserID, product.Name, product.Price, product.CategoryID, product.Condition, product.MinimumAge, product.MaximumAge, product.TargetGender, product.Description, product.Tags)
+	return err
 }
 
 func ProductGetByID(ctx context.Context, db database.DB, id string, requireApproval bool) (*Product, error) {
@@ -55,14 +103,17 @@ type ProductFilter struct {
 }
 
 func ProductList(ctx context.Context, db database.DB, requireApproval bool, limit int, offset int, filter *ProductFilter) ([]*Product, error) {
-	args := []any{requireApproval, limit, offset}
+	args := []any{limit, offset}
 	query := `
 		SELECT p.*, pc.name as category
 		FROM products p
 		LEFT JOIN product_categories pc
 			ON p.category_id = pc.id AND pc.deleted_at IS null
-		WHERE p.deleted_at IS null AND p.approved = $1
+		WHERE p.deleted_at IS null
 	`
+	if requireApproval {
+		query += " AND p.approved = true"
+	}
 	if filter != nil {
 		if filter.Name != "" {
 			args = append(args, filter.Name)
@@ -83,8 +134,8 @@ func ProductList(ctx context.Context, db database.DB, requireApproval bool, limi
 	}
 	query += `
 		ORDER BY p.created_at DESC
-		LIMIT $2
-		OFFSET $3
+		LIMIT $1
+		OFFSET $2
 	`
 	var products []*Product
 	err := pgxscan.Select(ctx, db, &products, query, args...)
